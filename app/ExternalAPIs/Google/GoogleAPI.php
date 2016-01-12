76479ee19fc6ea2129b00665b687805892f60e24
kevineger <egerkevinjames@gmail.com>
2016-01-12 11:19:17 -0800

    Restructures Google BigQuery API to use a service provider and bind a Facade. This allows for much more convenient, clear and modular calls to the Big Query API.

--- app/ExternalAPIs/Google/GoogleAPI.php
+++ app/ExternalAPIs/Google/GoogleAPI.php
@@ -24,8 +24,7 @@ class GoogleAPI {
         $bigquery = new Google_Service_Bigquery($this->client);
         $projectId = 'redditbigquery';
         $request = new Google_Service_Bigquery_QueryRequest();
-//        $request->setQuery('SELECT DAYOFWEEK(SEC_TO_TIMESTAMP(created - 60*60*5)) as sub_dayofweek, HOUR(SEC_TO_TIMESTAMP(created - 60*60*5)) as sub_hour, SUM(IF(score >= 3000, 1, 0)) as num_gte_3000, FROM [fh-bigquery:reddit_posts.full_corpus_201509] GROUP BY sub_dayofweek, sub_hour ORDER BY sub_dayofweek, sub_hour');
-        $request->setQuery($query);
+        $request->setQuery(config("constants.$query"));
         $response = $bigquery->jobs->query($projectId, $request);
 
         return $response->getRows();
